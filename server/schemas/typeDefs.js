0a39ba37f91276616fc88e86cf691698b3321cbf
Pfizz <pfiste80@gmail.com>
2021-11-03 21:23:19 -0400

    look up thought sby username 1.5

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,16 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs query called helloworld, type of data returned will be a string
 const typeDefs = gql`
+
+type Thought {
+  _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+}
 type Query {
-    helloWorld: String
+    thoughts: [Thought]
   }
 `;
 
