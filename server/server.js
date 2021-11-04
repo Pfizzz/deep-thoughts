b63a4afbfd0a1dfb070a8b92a7e274f984dcae09
Pfizz <pfiste80@gmail.com>
2021-11-04 16:37:21 -0400

    concurrently integrated, EO Lesson 3

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -16,12 +17,23 @@ const startServer = async () => {
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
+
 // Initialize the Apollo server
 startServer();
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
